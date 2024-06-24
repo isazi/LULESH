691d53b77116400dc1bbf48e2bbd9460440d68ea
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2024-06-24 15:40:14 +0200

    Added InitStressTermsForElems kernel.

--- openacc/src/tuning.py
+++ openacc/src/tuning.py
@@ -10,15 +10,17 @@ from kernel_tuner.utils.directives import (
     generate_directive_function,
     extract_directive_data,
     extract_preprocessor,
-    allocate_signature_memory
 )
 
 
 def command_line() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--length", help="Length", type=int, default=4096)
+    parser.add_argument("--length", help="length", type=int, default=8445)
+    parser.add_argument("--elems", help="numElem", type=int, default=27000)
+    parser.add_argument("--nodes", help="numNode", type=int, default=29791)
     parser.add_argument("--float", help="Use single precision", action="store_true")
     return parser.parse_args()
+
 
 arguments = command_line()
 
@@ -28,10 +30,13 @@ with open("lulesh.cc") as file:
 # LULESH compiler options
 compiler_options = [
     "-acc=gpu",
+    "-gpu=cc86",
     "-I.",
     "-mp",
+    "-lm",
     "-O2",
-    "-DUSE_MPI=0"
+    "-DUSE_MPI=0",
+    "-DSEDOV_SYNC_POS_VEL_LATE=1",
 ]
 # data types
 real_type = np.float64
@@ -40,25 +45,25 @@ if arguments.float:
     real_type = np.float32
     real_bytes = 4
 # preprocessor
-user_preprocessor = [f"#define length {arguments.length}\n", "#define emin Real_t(-1.0e+15)\n"]
+user_preprocessor = ["#define emin Real_t(-1.0e+15)\n"]
 
 # extracting tunable code
 app = Code(OpenACC(), Cxx())
 preprocessor = extract_preprocessor(source)
-preprocessor += user_preprocessor
 signatures = extract_directive_signature(source, app)
 functions = extract_directive_code(source, app)
 data = extract_directive_data(source, app)
 
 # CalcEnergyForElems_0
-print(f"Tuning CalcEnergyForElems_0")
+print("Tuning CalcEnergyForElems_0")
 code = generate_directive_function(
-    preprocessor,
+    preprocessor + user_preprocessor,
     signatures["CalcEnergyForElems_0"],
     functions["CalcEnergyForElems_0"],
     app,
-    data=data["CalcEnergyForElems_0"]
+    data=data["CalcEnergyForElems_0"],
 )
+user_preprocessor += [f"#define length {arguments.length}\n"]
 e_new = np.zeros(arguments.length).astype(real_type)
 e_old = np.random.rand(arguments.length).astype(real_type)
 p_old = np.random.rand(arguments.length).astype(real_type)
@@ -70,7 +75,9 @@ args = [e_new, e_old, p_old, q_old, delvc, work]
 tune_params = dict()
 tune_params["vlength"] = [32 * i for i in range(1, 33)]
 metrics = dict()
-metrics["GB/s"] = lambda p: (6 * real_bytes * arguments.length / 10**9) / (p["time"] / 10**3)
+metrics["GB/s"] = lambda p: (6 * real_bytes * arguments.length / 10**9) / (
+    p["time"] / 10**3
+)
 metrics["GFLOPS/s"] = lambda p: (6 * arguments.length / 10**9) / (p["time"] / 10**3)
 
 tune_kernel(
@@ -81,8 +88,41 @@ tune_kernel(
     tune_params,
     compiler_options=compiler_options,
     compiler="nvc++",
-    metrics=metrics
+    metrics=metrics,
 )
 
+# InitStressTermsForElems
+print("Tuning InitStressTermsForElems")
+code = generate_directive_function(
+    preprocessor,
+    signatures["InitStressTermsForElems"],
+    functions["InitStressTermsForElems"],
+    app,
+    data=data["InitStressTermsForElems"],
+)
+user_preprocessor += [
+    f"#define numElem {arguments.elems}\n",
+    f"#define numNode {arguments.nodes}\n",
+]
+p = np.random.rand(arguments.elems).astype(real_type)
+q = np.random.rand(arguments.elems).astype(real_type)
+sigxx = np.zeros(arguments.elems).astype(real_type)
+sigyy = np.zeros(arguments.elems).astype(real_type)
+sigzz = np.zeros(arguments.elems).astype(real_type)
+args = [p, q, sigxx, sigyy, sigzz]
 
+metrics["GB/s"] = lambda p: (5 * real_bytes * arguments.elems / 10**9) / (
+    p["time"] / 10**3
+)
+metrics["GFLOPS/s"] = lambda p: (2 * arguments.elems / 10**9) / (p["time"] / 10**3)
 
+tune_kernel(
+    "InitStressTermsForElems",
+    code,
+    0,
+    args,
+    tune_params,
+    compiler_options=compiler_options,
+    compiler="nvc++",
+    metrics=metrics,
+)
